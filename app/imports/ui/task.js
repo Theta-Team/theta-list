fd12aea56af48d998030c41f77c905db5210e20a
brandon-chun <bchun9@hawaii.edu>
2017-08-05 22:48:11 -1000

    removed autopublish package, added functionality of a private and public button

--- app/imports/ui/task.js
+++ app/imports/ui/task.js
@@ -5,6 +5,13 @@ import { Tasks } from '../api/tasks.js';
 
 import './task.html';
 
+Template.task.helpers({
+
+    isOwner() {
+        return this.owner === Meteor.userId();
+    },
+
+});
 Template.task.events({
     'click .toggle-checked'() {
 
@@ -13,7 +20,12 @@ Template.task.events({
     },
 
     'click .delete'() {
-        Meteor.call('tasks.remove', this._id);    },
+        Meteor.call('tasks.remove', this._id);
+    },
+
+    'click .toggle-private'() {
+        Meteor.call('tasks.setPrivate', this._id, !this.private);
+    },
 
 });
 
