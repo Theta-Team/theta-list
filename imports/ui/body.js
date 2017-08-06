5f94ddb411e6018ee3118b0eea384ae0dd7d1130
Brian <blogan7@hawaii.edu>
2017-08-05 19:02:01 -1000

    Add files via upload

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,7 +5,6 @@ import { Tasks } from '../api/tasks.js';
 import './task.js';
 import './body.html';
 
-/**
 Template.body.onCreated(function bodyOnCreated() {
     this.state = new ReactiveDict();
 });
@@ -61,48 +60,3 @@ Template.body.events({
     },
 
 });
-*/
-
-
-
-import { ReactiveVar } from 'meteor/reactive-var';
-//import { Session } from 'meteor/session';
-
-import './task.js';
-import './todo.html';
-import './timer.js'
-
-Template.todo.helpers({
-  tasks() {
-    // Show newest tasks at the top
-    return Tasks.find({}, { sort: { createdAt: -1 } });
-  },
-});
-
-Template.todo.events({
-  'submit .new-task'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
-
-    console.log(event);
-
-    // Get value from form element
-    const target = event.target;
-
-    const title = target.title.value;
-    const description = target.description.value;
-
-    // Insert a task into the collection
-    Tasks.insert({
-      title,
-      description,
-      cycles: 0,
-      createdAt: new Date(), // current time
-    });
-
-    // Clear form
-    target.title.value = '';
-    target.description.value = '';
-  },
-});
-
